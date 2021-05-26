71936f6601baaccfaf6ecc5fb79b945c5c52ff8f
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-26 17:01:56 +0800

    lesson 06 - blogs carousel (owl carousel)

--- index.js
+++ index.js
@@ -40,4 +40,22 @@ $(document).ready(function() {
     $grid.isotope({ filter: filterValue});
   });
 
+  // blogs owl carousel
+  $("#blogs .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive : {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+})
+
 });
