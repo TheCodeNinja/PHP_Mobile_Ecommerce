1452e4fd0b2a26c2be82ddebca0ab5a6708dc259
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-26 16:22:04 +0800

    lesson 04 - filter special products (use isotope)

--- index.js
+++ index.js
@@ -27,4 +27,17 @@ $(document).ready(function() {
     }
   });
 
+  // set up isotope layout options
+  var $grid = $(".grid").isotope({
+    itemSelector : '.grid-item',
+    layoutMode : 'fitRows'
+  });
+
+  // click event listener
+  $(".button-group").on("click", "button", function() {
+    var filterValue = $(this).attr('data-filter');
+    console.log(filterValue);
+    $grid.isotope({ filter: filterValue});
+  });
+
 });
