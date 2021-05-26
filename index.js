6276077a2d7d5b983f92054522f0ad57b3ab66e4
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-26 23:05:19 +0800

    lesson 11 - js for changing value of quantity input (the selected input only)

--- index.js
+++ index.js
@@ -61,10 +61,11 @@ $(document).ready(function() {
   // product qty section
   let $qty_up = $(".qty .qty-up");
   let $qty_down = $(".qty .qty-down");
-  let $input = $(".qty .qty_input");
+  // let $input = $(".qty .qty_input");
 
   // click on qty up button
   $qty_up.click(function(e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
     if ($input.val() >= 1 && $input.val() <= 9) {
       $input.val(function(i, oldval) {
         return ++oldval;
@@ -74,6 +75,7 @@ $(document).ready(function() {
 
   // click on qty down button
   $qty_down.click(function(e) {
+    let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
     if ($input.val() > 1 && $input.val() <= 10) {
       $input.val(function(i, oldval) {
         return --oldval;
@@ -81,5 +83,14 @@ $(document).ready(function() {
     }
   });
 
+  /** 
+  <div data-role="page" data-last-value="43" data-hidden="true" data-options='{"name":"John"}'></div>
+
+  $( "div" ).data( "role" ) === "page";
+  $( "div" ).data( "lastValue" ) === 43;
+  $( "div" ).data( "hidden" ) === true;
+  $( "div" ).data( "options" ).name === "John";
+
+  */
 
 });
