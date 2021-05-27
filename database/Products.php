497b9a5a24354a9595e881f74ee7b1ca569e6dd7
Mario Ninja <mario_ninja@Marios-MacBook-Pro.local>
2021-05-27 19:55:14 +0800

    lesson 16 - fetch data for cart page and display the data

--- database/Products.php
+++ database/Products.php
@@ -11,6 +11,10 @@ class Products {
     $this->db = $db;
   }
 
+  /**
+   * DB handlers
+   */
+
   // to fetch products
   public function getData($table = 'product') {
     $result = $this->db->connection->query("SELECT * FROM {$table}");
@@ -23,5 +27,21 @@ class Products {
     }
 
     return $resultArray;
+  }
+
+  // get product by id
+  public function getProduct($item_id=null, $table='product') {
+    if (isset($item_id)) {
+      $result = $this->db->connection->query("SELECT * FROM {$table} WHERE item_id={$item_id}");
+
+      $resultArray = array();
+
+      // fetch product data one by one
+      while ($item = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        $resultArray[] = $item;
+      }
+
+      return $resultArray;
+    }
   }
 }
--- templates/_cart.php
+++ templates/_cart.php
@@ -6,13 +6,21 @@
     <div class="row">
       <!-- cart items -->
       <div class="col-sm-9">
+        <?php
+          foreach ($products->getData('cart') as $cartItem) :
+            $product = $products->getProduct($cartItem['item_id']);
+            // print_r($product);
+
+            // store the new 'returned array' to 'subTotal'
+            $subTotal[] = array_map(function($cartProduct) {
+        ?>
         <div class="row border-top py-3 mt-3">
           <div class="col-sm-2">
-            <img src="./assets/products/1.png" style="height: 120px;" alt="cart1" class="img-fluid">
+            <img src="<?php echo $cartProduct['item_image'] ?? "./assets/products/1.png" ?>" style="height: 120px;" alt="cart1" class="img-fluid">
           </div>
           <div class="col-sm-8">
-            <h5 class="font-baloo font-size-20">Samsung Galaxy 10</h5>
-            <small>by Samsung</small>
+            <h5 class="font-baloo font-size-20"><?php echo $cartProduct['item_name'] ?? "Unknown"; ?></h5>
+            <small>by <?php echo $cartProduct['item_brand'] ?? "Brand"; ?></small>
             <div class="d-flex">
               <div class="rating text-warning font-size-12">
                 <span><i class="fas fa-star"></i></span>
@@ -39,48 +47,16 @@
           </div>
           <div class="col-sm-2 text-right">
             <div class="font-size-20 text-danger font-baloo">
-              $<span class="product_price">152</span>
+              $<span class="product_price"><?php echo $cartProduct['item_price'] ?? 0; ?></span>
             </div>
           </div>
         </div>
-
-        <div class="row border-top py-3 mt-3">
-          <div class="col-sm-2">
-            <img src="./assets/products/2.png" style="height: 120px;" alt="cart1" class="img-fluid">
-          </div>
-          <div class="col-sm-8">
-            <h5 class="font-baloo font-size-20">Samsung Galaxy 10</h5>
-            <small>by Samsung</small>
-            <div class="d-flex">
-              <div class="rating text-warning font-size-12">
-                <span><i class="fas fa-star"></i></span>
-                <span><i class="fas fa-star"></i></span>
-                <span><i class="fas fa-star"></i></span>
-                <span><i class="fas fa-star"></i></span>
-                <span><i class="far fa-star"></i></span>
-              </div>
-              <a href="#" class="px-2 font-rale font-size-14">20,534 ratings</a>
-            </div>
-            <div class="qty d-flex pt-2">
-              <div class="d-flex font-rale w-25">
-                <button class="qty-up border bg-light">
-                  <i class="fas fa-angle-up"></i>
-                </button>
-                <input type="text" class="qty_input border px-2 w-100 bg-light" disabled value="1" placeholder="1">
-                <button class="qty-down border bg-light">
-                  <i class="fas fa-angle-down"></i>
-                </button>
-              </div>
-              <button type="submit" class="btn font-baloo text-danger px-3 border-right">Delete</button>
-              <button type="submit" class="btn font-baloo text-danger">Save for Later</button>
-            </div>
-          </div>
-          <div class="col-sm-2 text-right">
-            <div class="font-size-20 text-danger font-baloo">
-              $<span class="product_price">152</span>
-            </div>
-          </div>
-        </div>
+        <?php
+              return $cartProduct['item_price'];
+            }, $product); // closing array_map function
+            // print_r($subTotal);
+          endforeach;
+        ?>
       </div>
 
       <!-- subtotal -->
@@ -91,7 +67,10 @@
           </h6>
           <div class="border-top py-4">
             <h5 class="font-baloo font-size-20">
-              Subtotal (2 item):&nbsp; <span class="text-danger">$<span id="deal-price">152.00</span> </span> 
+              Subtotal (<?php echo count($subTotal) ?? 0; ?>  item):&nbsp; 
+              <span class="text-danger">
+                $<span id="deal-price"><?php echo isset($subTotal) ? $cart->getSum($subTotal) : 0; ?></span>
+              </span> 
             </h5>
             <button type="submit" class="btn btn-warning mt-3">Proceed to Buy</button>
           </div>
